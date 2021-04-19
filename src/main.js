62e82eb245e1b7fdce6f1db0f8e6b00e0d74e0e2
Alejandro Silva-Serrano <asilvase@ucsc.edu>
2021-04-19 11:52:25 -0700

    explosions working

--- src/main.js
+++ src/main.js
@@ -11,4 +11,7 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-let starSpeed = 4;
+let starSpeed = 2.5;
+
+// reserve keyboard bindings
+let keyF, keyR, keyLEFT, keyRIGHT;
