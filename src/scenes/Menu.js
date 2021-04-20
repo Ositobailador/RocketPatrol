b564d7cfecc326aaf3c96806c4fac58b264bf7d1
Alejandro Silva-Serrano <asilvase@ucsc.edu>
2021-04-19 18:40:31 -0700

    fixed spelling

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -33,7 +33,7 @@ class Menu extends Phaser.Scene {
         menuConfig.backgroundColor = '#00ff00';
         menuConfig.color = '#000';
         this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize 
-            + borderPadding, 'Press <- for Novice or -> fro Expert', menuConfig).setOrigin(0.5);
+            + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
         
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
