44b855f5e33b140a90c16c5059a973ba9730d1a2
Alejandro Silva-Serrano <asilvase@ucsc.edu>
2021-06-08 01:28:50 -0700

    created new title sceen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,15 +8,30 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+
+        // load background image
+        this.load.image('starfield', './assets/starfield.png');
+
+        // load menu images
+        this.load.image('rocketShip', './assets/cartoon_rocket.png');
+        this.load.image('planet', './assets/cartoon_planet.png');
     }
     
     create() {
+        // add background image
+        this.add.tileSprite(0, 0, game.config.wdth, game.config.height, 'starfield').setOrigin(0, 0);
+
+        // add rocket Ship
+        this.add.tileSprite(game.config.width / 1.25, borderUISize * 2.5, 220, 124, 'rocketShip').setOrigin(0.5, 0.5);
+        // add planet 
+        this.add.tileSprite(borderUISize * 3.2, borderUISize * 3.5, 220, 208, 'planet').setOrigin(0.5, 0.5);
+
         // menu text configuration
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            fontFamily: 'Oswaldo',
+            fontSize: '34px',
+            backgroundColor: 'red',
+            color: 'blue',
             allign: 'right',
             padding: {
                 top: 5,
@@ -26,14 +41,19 @@ class Menu extends Phaser.Scene {
         }
 
         // show menu text
-        this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 
+        this.add.text(game.config.width / 2, game.config.height / 4 - borderUISize - borderPadding, 
             'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 2, 
-            'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 10, game.config.height / 1.38, 
+            'Keys :', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 3, game.config.height / 1.2, 
+            'Use <--> arrows to move', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 4.3, game.config.height / 1.1, 
+            'Press (F) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00ff00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize 
-            + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
+        menuConfig.color = '#FF6700';
+        this.add.text(game.config.width / 2, game.config.height / 2.1, 
+            'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2.5, 'To Start:', menuConfig).setOrigin(0.5);
         
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
